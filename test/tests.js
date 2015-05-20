9dee2769687db069138aa26429d45ca558e7f707
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2015-05-20 16:41:25 +0200

    add linux tar

--- test/tests.js
+++ test/tests.js
@@ -1,41 +1,33 @@
 /**
- * Nodeunit functional tests.  Requires internet connection to validate phantom
+ * Nodeunit functional tests.  Requires internet connection to validate wkhtmltopdf
  * functions correctly.
  */
 
 var childProcess = require('child_process');
 var fs = require('fs');
 var path = require('path');
-var phantomjs = require('../');
+var htmltopdf = require('../');
 
 
 exports.testDownload = function (test) {
   test.expect(1);
-  test.ok(fs.existsSync(phantomjs.path), 'Binary file should have been downloaded');
+  test.ok(fs.existsSync(htmltopdf.path), 'Binary file should have been downloaded');
   test.done();
 };
 
 
-exports.testPhantomExecutesTestScript = function (test) {
-  test.expect(1);
+exports.testHtmlToPdfTransformation = function (test) {
+  test.expect(2);
 
   var childArgs = [
-    path.join(__dirname, 'loadspeed.js'),
-    'http://www.google.com/'
+    path.join(__dirname, "test.html"),
+    path.join(__dirname, "test.pdf"),
   ];
 
-  childProcess.execFile(phantomjs.path, childArgs, function (err, stdout, stderr) {
-    var value = (stdout.indexOf('msec') !== -1);
-    test.ok(value, 'Test script should have executed and returned run time');
-    test.done();
-  });
-};
-
-
-exports.testPhantomExitCode = function (test) {
-  test.expect(1);
-  childProcess.execFile(phantomjs.path, [path.join(__dirname, 'exit.js')], function (err, stdout, stderr) {
-    test.equals(err.code, 123, 'Exit code should be returned from phantom script');
+  childProcess.execFile(htmltopdf.path, childArgs, function (err, stederr, stdout) {
+    var value = (stdout.indexOf('Done') !== -1);
+    test.ok(value, 'Test script should have executed and returned string Done');
+    test.ok(fs.existsSync(path.join(__dirname, "test.pdf")), 'Test should have created a test.pdf');
     test.done();
   });
 };
@@ -44,10 +36,10 @@ exports.testPhantomExitCode = function (test) {
 exports.testBinFile = function (test) {
   test.expect(1);
 
-  var binPath = phantomjs.path;
+  var binPath = htmltopdf.path;
 
   childProcess.execFile(binPath, ['--version'], function (err, stdout, stderr) {
-    test.equal(phantomjs.version, stdout.trim(), 'Version should be match');
+    test.ok(stdout.trim().indexOf(htmltopdf.version) > -1, 'Version should be match');
     test.done();
   });
 };
